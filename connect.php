f1a3693952140b7ff1c24e46b778f15eb41a2bfe
Jeevithan42 <145555101+Jeevithan42@users.noreply.github.com>
2024-08-20 19:36:02 -0400

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,3 +1,6 @@
+
+
+
 <?php
 
 $Name = $_POST['Name'];
@@ -11,21 +14,23 @@ $file = $_POST['myfile'];
 $connect = new mysqli('localhost', 'SAC',  'CLUBHUB_24', 'clubhub');
 
 if($connect -> connect_error){
-    // $error_type = $connect->connect_error;
-    // echo "<script>var error = '$error_type';</script>";
-    // echo '<script>alert("Database Connection Failed: " + error);</script>';
+    $error_type = $connect->connect_error;
+    echo "<script>var error = '$error_type';</script>";
+    echo '<script>alert("Database Connection Failed: " + error);</script>';
     die("failed");
 }
 
 else{
-// $smth = $connect->prepare("insert into customers(Name, Description, Meeting, email, image) values (?, ?, ?, ?, ?)");
-// $smth->bind_param("ssssb", $Name, $Description, $Meeting, $email, $myfile);
-// echo '<script>alert("Entry added sucessfully! Yippee! I like men!");</script>';
-// $smth->close();
-// $connect->close();
+$smth = $connect->prepare("insert into customers(Name, Description, Meeting, email, image) values (?, ?, ?, ?, ?)");
+$smth->bind_param("ssssb", $Name, $Description, $Meeting, $email, $myfile);
+echo '<script>alert("Entry added sucessfully! Yippee! I like men!");</script>';
+$smth->close();
+$connect->close();
 die("yay?");
 
 }
 
 
 ?>
+
+
