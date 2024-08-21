4d33edaacb5cb2e67fd6728f4a891fdb1b9a5de0
Jeevithan42 <145555101+Jeevithan42@users.noreply.github.com>
2024-08-21 08:58:22 -0400

    DISPLAYS INFORMATION FROM DATABASE AS WELL

---
+++ Showclubs.php
@@ -0,0 +1,60 @@
+<?php
+$connect = new mysqli('localhost', 'root',  '', 'clubhub','3306');
+
+
+if($connect -> connect_error){
+    $error_type = $connect->connect_error;
+    echo "<script>var error = '$error_type';</script>";
+    echo '<script>alert("Database Connection Failed: " + error);</script>';
+    die("failed");
+}
+
+
+$sql = 'SELECT name, description, meeting, image FROM ClubRegistry';
+$result = mysqli_query($connect, $sql) or die( mysqli_error($connect));
+
+
+// fetch the resulting rows as an array
+$clubs = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free the $result from memory (good practise)
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($connect);
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+<h2>Form?</h2>
+
+</head>
+
+<body>
+
+<div class ="club_page">
+<h3 class="Title">Clubs Here at Cameron</h4>
+  <div class="Table">
+      <div class="row">
+          <?php foreach($clubs as $club){ ?>
+              <div class="col s6 md3">
+                  <div class="card z-depth-0">
+                      <div class="card-content center">
+                          <h6 class="club_text"> Name: <?php echo $club['name']; ?></h6>
+                          <h6 class="'club_text">Description: <?php echo $club['description']; ?></h6>
+                          <h6 class="'club_text">Meeting: <?php echo $club['meeting']; ?></h6>
+                          <h6 class="'club_text">Image: <?php echo $club['image']; ?></h6>
+                      </div>
+                  </div>
+              </div>
+          <?php } ?>
+      </div>
+  </div>
+</div>
+</body>
+
+</html>
---
+++ Submission.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<h2>Form?</h2>
+
+</head>
+
+<body>
+
+    <form action="connect.php" method="post" class="submission">
+      <h3>idk</h3>
+      <br>
+      <h5>*Required</h5>
+      <br>
+      <label class="label" for="Name">*Club Name:</label>
+      <input class="input_box" type="text" id="Name" name="Name"><br><br>
+  
+      <label class="label" for="Description" aria-placeholder="500 Characters">*Description:</label>
+      <input class="input_box" type="text" id="Description" name="Description"><br><br>
+  
+      <label class="label" for="Meeting">*Meeting:</label>
+      <input class="input_box" type="text" id="Meeting" name="Meeting"><br><br>
+  
+      <label class="label" for="email">*Email:</label>
+      <input class="input_box" type="email" id="email" name="email"><br><br>
+  
+      <label class="label" for="myfile">Select an Image:</label>
+      <input class="input_box" type="file" id="myfile" name="myfile"><br><br>
+  
+      <button type="submit" class="btn" id="Submit">Submit</button>
+      <button type="button" class="btn" id="Update">Update</button>
+      <button type="button" class="btn" id="Show">Show Table</button>
+      <br>
+  
+  
+  </form>
+
+
+
+</body>
+
+</html>
